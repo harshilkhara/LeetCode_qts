abf01d08b09f5457fe9f37ff5489068eb33897ab
Harshil Khara <50494307+harshilkhara@users.noreply.github.com>
2020-04-10 20:14:18 +0530

    Add files via upload

---
+++ roman.py
@@ -0,0 +1,38 @@
+def value (r):
+	if (r=="I"):
+		return 1
+	if (r=="V"):
+		return 5
+	if (r=="X"):
+		return 10
+	if (r=="L"):
+		return 50
+	if (r=="C"):
+		return 100
+	if (r=="D"):
+		return 500
+	if (r=="M"):
+		return 1000
+
+def romantoint(string):
+	res=0
+	i=0
+	while(i<(len(string))):
+		s1=value(string[i])
+		if (i+1 < len(string)):
+			s2=value(string[i+1])
+			if (s1>=s2):
+				res=res+s1
+				i=i+1
+			else:
+				res=res+s2-s1
+				i=i+2
+		else:
+			res=res+s1
+			i=i+1
+	return res
+
+str=input("Enter a Roman Numeral: ")
+string=str.upper()
+result=romantoint(string)
+print(result)	
