c4ffd98523a5ab2011533a78c88a54ec4cf70336
Harshil Khara <kharaharshil@gmail.com>
2021-10-31 23:55:02 -0400

    added brute forcemethod and time complexity

--- twosum.py
+++ twosum.py
@@ -1,4 +1,4 @@
-def twosum(nums,target):
+def twosum(nums,target):     #Optimal method, use hash maps------Time complexity O(n)
     complementMap=dict()
     for i in range(len(nums)):
         num=nums[i]
@@ -6,9 +6,21 @@ def twosum(nums,target):
         if num in complementMap:
             return[complementMap[num],i]
         else:
-            complementMap[complement]=i
+            complementMap[complement]=i     #adding into dict()
                 
+
+def brute(nums,target):    #brute force method   ---- Time complexity O(n^2)
+
+    for i in range(n):        
+        for j in range(1,n):
+            sum= nums[i]+nums[j]
+            if sum==target:
+                return [i,j],nums[i],nums[j]
+
+            
 nums=[1,2,3,4,5]
 target=8
-twosum(nums,target)
+n=len(nums)
+print(n)
 print(twosum(nums,target))
+print(brute(nums,target))
