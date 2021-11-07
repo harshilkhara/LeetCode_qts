8aa5e0ded7f6efac9c92d25532eb7431d017a9ac
Harshil Khara <kharaharshil@gmail.com>
2021-11-07 09:47:27 -0500

    added twosum1 for easy understanding

--- twosum.py
+++ twosum.py
@@ -1,3 +1,15 @@
+#Most efficent one:      Most optimum found----Time complexity O(n)
+#in terms of understanding it is easy
+def twosum1(nums,target):
+    h={}         #nothing but creating dict()
+    for i in range(len(nums)):
+        complement=target-nums[i]
+        if complement in h:          
+            return [h[complement],i]
+        h[nums[i]]=i                      #here it does not create complement map but duplicates 
+                                          #the list
+ 
+
 def twosum(nums,target):     #Optimal method, use hash maps------Time complexity O(n)
     complementMap=dict()
     for i in range(len(nums)):
@@ -12,7 +24,7 @@ def twosum(nums,target):     #Optimal method, use hash maps------Time complexity
 def brute(nums,target):    #brute force method   ---- Time complexity O(n^2)
 
     for i in range(n):        
-        for j in range(1,n):
+        for j in range(i+1,n):
             sum= nums[i]+nums[j]
             if sum==target:
                 if i==j:
@@ -21,8 +33,9 @@ def brute(nums,target):    #brute force method   ---- Time complexity O(n^2)
 
             
 nums=[1,2,3,4,5]
-target=6
+target=8
 n=len(nums)
 print(n)
+print(twosum1(nums,target))
 print(twosum(nums,target))
 print(brute(nums,target))
