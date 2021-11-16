bc6e41edd191cb421fd4a7c712e49e5af200896f
Harshil Khara <kharaharshil@gmail.com>
2021-11-16 14:40:14 -0500

    again modified sqrt.py

--- sqrt.py
+++ sqrt.py
@@ -1,11 +1,11 @@
-def mySqrt(x):
+def mySqrt(x):          #Time complexity- O(log x) (Binary search tree)
     if x==0 or x==1:
         return x
     start=1
     end=x//2
     while start<=end:
-        mid=start+(end-start)//2  #To avoid overflow we use start+(end-start)//2 instead of 
-        if mid > x/mid:                                #simple (start+end)//2
+        mid=start+(end-start)//2 #To avoid overflow we use start+(end-start)//2 instead of simple (start+end)//2
+        if mid > x/mid:      # which is same as mid*mid>x:                          
             end=mid-1
         else:
             start=mid+1
