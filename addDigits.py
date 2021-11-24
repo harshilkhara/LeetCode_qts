2f2566dee6a579a771bf57cf6c074e7ce5b8f57b
harshil <kharaharshil@gmail.com>
2021-11-23 19:10:54 -0500

    Added one more method in addDigits.py

--- addDigits.py
+++ addDigits.py
@@ -8,6 +8,15 @@ def addDigits(num): #Itereative method O(n)
 			sum=0
 	return sum
 
+def addDigits2(num):
+	while num > 9:
+		sum=0
+		while num:
+			sum+=num%10
+			num//=10
+		num=sum
+	return num
+
 def addDigits1(num):      #Fastest way TC O(1) SC O(1)
 	#return num%9 or 9 if num else 0
 	if num:
@@ -18,3 +27,5 @@ def addDigits1(num):      #Fastest way TC O(1) SC O(1)
 num=int(input("Enter the number: "))
 print(addDigits(num))
 print(addDigits1(num))
+print(addDigits2(num))
+
