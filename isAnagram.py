7acb9e3e4892c9288b22bc9e02f725e2d890a625
Harshil Khara <kharaharshil@gmail.com>
2021-11-26 20:38:55 -0500

    Added isAnagram.py

---
+++ isAnagram.py
@@ -0,0 +1,34 @@
+def isAnagram(s,t):    # TC O(N)  // SC O(N)
+
+	if len(s)!=len(t):
+		return False
+
+	hash,hash1={},{}
+	for i in s:
+		if i in hash:
+			hash[i]+=1
+		else:
+			hash[i]=1
+
+	for j in t:
+		if j in t:
+			hash[j]+=1
+		else:
+			hash[j]=1
+
+	if hash==hash1:
+		return True
+	return False
+
+def isAnagram1(s,t):  # TC O(nlogn)  // SC O(1)
+	if len(s)!=len(t):
+		return False
+	a=sorted(s)
+	b=sorted(t)
+	if a==b:
+		return True
+	return False
+
+
+print(isAnagram("anagram","nagaram"))
+print(isAnagram("car","rat"))
