81949bb03bb3c301733d4ea65a71528c1d01b176
Harshil Khara <kharaharshil@gmail.com>
2021-12-06 17:34:17 -0500

    Added comments in complexnum.py

--- complexnum.py
+++ complexnum.py
@@ -1,9 +1,12 @@
 def complexNumberMultiply(num1,num2):  # TC O(1) // SC O(1)
 	a,b=num1.split("+")
 	c,d=num2.split("+")
-	b,d=b[:-1],d[:-1]
+	b,d=b[:-1],d[:-1] # [start:end:step]   [:-1] --> means start from 0, - means start from end of array
+	                  # end is excluded in slicing
 
-	x,y,z=int(a)*int(c),int(b)*int(d),int(a)*int(d)+int(b)*int(c)
+    # x=a*c=real, y=b*d=real, z=a*d+b*c=img
+
+	x,y,z=int(a)*int(c),int(b)*int(d),int(a)*int(d)+int(b)*int(c) 
 	real=x-y
 	img=z
 
