65cbf8ca10e74e58b258af236555e84af3cf1a5d
Harshil Khara <kharaharshil@gmail.com>
2021-12-26 15:35:05 -0500

    Added thirdMax.py

---
+++ thirdMax.py
@@ -0,0 +1,26 @@
+def thirdMax(nums):    # TC O(n) // SC O()
+	list(set(nums))
+	n=len(nums)
+	if n <3:
+		return(max(nums))
+	else:
+		nums.remove(max(nums))
+		nums.remove(max(nums))
+		return max(nums)
+
+import heapq      
+def thirdMax1(nums):
+    heap = list()
+    heapq.heapify(heap)
+    for num in nums:
+        if num not in heap:
+            heapq.heappush(heap, num)
+            if len(heap) > 3:
+                heapq.heappop(heap)
+    if len(heap) == 2:
+        heapq.heappop(heap)
+    return heapq.heappop(heap)
+
+
+print(thirdMax([3,2,5,6,12]))
+print(thirdMax1([3,2,5,6,12]))
