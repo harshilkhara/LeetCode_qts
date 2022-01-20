8accd1247107f3f6fe51ccbafa31a56b6f12db15
Harshil Khara <kharaharshil@gmail.com>
2022-01-20 16:47:58 -0500

    Edited addBinary.py

--- addBinary.py
+++ addBinary.py
@@ -30,14 +30,11 @@ def addBinary1(a,b):          # TC O(n+m)
 def addBinary2(a,b):               # TC O(n+m) // SC O(max(n,m))
      x, y = int(a, 2), int(b, 2)
      while y:
-         answer = x ^ y
-         carry = (x & y) << 1
-         x, y = answer, carry
+         answer = x ^ y            # XOR ( ^ )returns 1 when bits are different else 0
+         carry = (x & y) << 1      # AND ( & )returns 1 when both bits are 1 else 0
+         x, y = answer, carry      # << 1 --> leftshift bit by 1
          #x,y= x ^ y, (x & y) <<1
      return bin(x)[2:]
-
-
-
 
 a=input("Enter first binary string: ")
 b=input("Enter second binary string: ")
