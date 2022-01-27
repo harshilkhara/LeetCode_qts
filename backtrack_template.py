9b1ae34de724a0b5765808d926ad9d1b363563ce
Harshil Khara <kharaharshil@gmail.com>
2022-01-26 20:02:56 -0500

    Added backtrack_template.py

---
+++ backtrack_template.py
@@ -0,0 +1,15 @@
+def backtrack(candidate):
+    if find_solution(candidate):
+        output(candidate)
+        return
+
+    # iterate all possible candidates.
+    for next_candidate in list_of_candidates:
+        if is_valid(next_candidate):
+            # try this partial candidate solution
+            place(next_candidate)
+            # given the candidate, explore further.
+            backtrack(next_candidate)
+            # backtrack
+            remove(next_candidate)
+            
