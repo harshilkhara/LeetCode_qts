e549305de0122e5e0485e0d0c7a036257540ef26
Harshil Khara <kharaharshil@gmail.com>
2022-01-27 23:40:01 -0500

    Added minSubarrayLen.py

---
+++ minSubarrayLen.py
@@ -0,0 +1,16 @@
+'''
+Approach- Sliding Window 
+
+'''
+def minSubarrayLen(target,nums): # TC O(2n) --> TC O(n) // SC O(1)
+	sum,l=0,0
+	ans=float('inf')
+	for i in range(len(nums)):
+		sum+=nums[i]
+		while sum>=target:
+			ans=min(ans,i-l+1)
+			sum-=nums[l]
+			l+=1
+	return 0 if ans==float('inf') else ans 
+
+print(minSubarrayLen(7,[2,3,1,2,4,3]))
