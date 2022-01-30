b3a0e3b617e64abbc274bf240ecbf2ecd25adfef
Harshil Khara <kharaharshil@gmail.com>
2022-01-29 21:41:23 -0500

    Edited minSubarrayLen.py

--- minSubarrayLen.py
+++ minSubarrayLen.py
@@ -5,10 +5,10 @@ Approach- Sliding Window
 def minSubarrayLen(target,nums): # TC O(2n) --> TC O(n) // SC O(1)
 	sum,l=0,0
 	ans=float('inf')
-	for i in range(len(nums)):
-		sum+=nums[i]
+	for r in range(len(nums)):
+		sum+=nums[r]
 		while sum>=target:
-			ans=min(ans,i-l+1)
+			ans=min(ans,r-l+1)
 			sum-=nums[l]
 			l+=1
 	return 0 if ans==float('inf') else ans 
