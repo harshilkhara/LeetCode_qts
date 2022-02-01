645fb8d3dc3da1c4a15ede610d1863d3c007196e
Harshil Khara <kharaharshil@gmail.com>
2022-02-01 15:44:12 -0500

    Added findMedianSortedArrays.py

---
+++ findMedianSortedArrays.py
@@ -0,0 +1,33 @@
+def findMedianSortedArrays(nums1,nums2): # TC O(log(min(m,n))) // SC O(1)
+	A,B=nums1,nums2
+	total=len(nums1)+len(nums2)
+	half=total//2
+
+	if len(B)<len(A):
+		A,B=B,A 
+
+	l,r=0,len(A)-1
+
+	while True:
+		i=(l+r)//2 #A
+		j=half-i-2 #B
+
+		Aleft=A[i] if i>=0 else float('-inf')
+		Aright=A[i+1] if (i+1)<len(A) else float('inf')
+		Bleft=B[j] if j>=0 else float('-inf')
+		Bright=B[j+1] if (j+1)<len(B) else float('inf')
+
+		# partition is correct
+		if Aleft <=Bright and Bleft<=Aright:
+			# odd 
+			if total%2:
+				return min(Aright,Bright)
+			# even
+			return (max(Aleft,Bleft)+min(Aright,Bright))/2
+		# partition is not correct
+		elif Aleft>Bright:  # Bright is the key
+			r=i-1
+		else:
+			l=i+1
+
+print(findMedianSortedArrays([1,2,3],[4,5,7]))
