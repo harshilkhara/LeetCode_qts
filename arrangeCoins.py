26943b424b54783710e3252270357ab4d9a3c267
Harshil Khara <kharaharshil@gmail.com>
2022-03-10 20:08:03 -0500

    Added arrangeCoins.py

---
+++ arrangeCoins.py
@@ -0,0 +1,22 @@
+#Approach 1- Binary Search
+def arrangeCoins(n): # TC O (log n) // SC O(1)
+	l,r=0,n 
+	while l<=r:
+		mid=(l+r)//2
+		coins=mid*(mid+1)//2
+		if coins==n:
+			return mid 
+		elif n < coins: # n is target
+			r=mid-1
+		else:
+			l=mid+1
+	return r 
+
+#Approach 2- Math 
+def arrangeCoins1(n): # TC O(1) // SC O(1)
+	return (int) ((2*n+0.25)**0.5-0.5)
+
+
+print(arrangeCoins(8))
+print(arrangeCoins1(16))
+
