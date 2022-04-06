f09f122092431bb9813d52e48273e8e05c3e0ea4
Harshil Khara <kharaharshil@gmail.com>
2022-04-05 20:30:54 -0400

    Edited canJump.py

--- canJump.py
+++ canJump.py
@@ -1,9 +1,10 @@
 # Approach- Greedy 
 def canJump(nums): # TC O(n) // SC O(1)
-	lastPos=len(nums)-1
+    lastPos=len(nums)-1
     for i in range(len(nums)-1,-1,-1):
         if i + nums[i]>=lastPos:
             lastPos=i
     return lastPos==0
 
 print(canJump([2,3,1,1,4]))
+print(canJump([3,2,1,0,4]))
