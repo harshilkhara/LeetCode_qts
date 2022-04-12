643cd475f33bbcd65989a86f8856ff65623fad9e
Harshil Khara <kharaharshil@gmail.com>
2022-04-12 11:56:40 -0400

    Added reverseString.py

--- reverseString.py
+++ reverseString.py
@@ -7,6 +7,14 @@ def reverseString(s): # TC O(n) // SC O(1)
 		r-=1
 	return s 
 
+def reverseString1(s): # TC O(n) // SC O(n)
+	n=len(s)
+	result=[]
+	for i in range(n-1,-1,-1):
+		result.append(s[i])
+	return result
+
 print(reverseString(["h","e","l","l","o"]))
+print(reverseString1(["h","e","l","l","o"]))
 
 
