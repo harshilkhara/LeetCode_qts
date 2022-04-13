d1091eb6cb5180cb35a74d3d31d92c3b13c290bc
Harshil Khara <kharaharshil@gmail.com>
2022-04-13 19:47:20 -0400

    Edited coinChange.py

--- coinChange.py
+++ coinChange.py
@@ -8,4 +8,15 @@ def coinChange(coins,amount): # TC O(amount*len(coins)) // SC O(amount)
 	            dp[a]=min(dp[a],1+dp[a-c])
 	return dp[amount] if dp[amount]!=amount+1 else -1
 
+# Leetcode solution with flipped for loop and no if condition 
+def coinChange1(coins,amount):
+	dp = [float('inf')] * (amount + 1)
+	dp[0] = 0
+
+	for coin in coins:
+	    for x in range(coin, amount + 1):
+	        dp[x] = min(dp[x], dp[x - coin] + 1)
+	return dp[amount] if dp[amount] != float('inf') else -1 
+
 print(coinChange([1,2,5],11))
+print(coinChange1([1,3,4,5],7))
