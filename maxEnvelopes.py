292c2e0f9bb191b7a5329c43e6e953d3c0d1b8ac
harshilkhara <kharaharshil@gmail.com>
2022-05-25 15:43:50 -0400

    Added maxEnvelopes.py

---
+++ maxEnvelopes.py
@@ -0,0 +1,18 @@
+# Approach 1- Sort + Longest Increasing Subsequence (LIS)
+from bisect import bisect_left
+def maxEnvelopes(arr): # TC O(n log n) // SC O(n)
+	arr.sort(key=lambda x: (x[0], -x[1]))
+
+	def lis(nums):
+	    dp = []
+	    for i in range(len(nums)):
+	        idx = bisect_left(dp, nums[i])
+	        if idx == len(dp):
+	            dp.append(nums[i])
+	        else:
+	            dp[idx] = nums[i]
+	    return len(dp)
+	# extract the second dimension and run the LIS
+	return lis([i[1] for i in arr])
+
+print(maxEnvelopes([[5,4],[6,4],[6,7],[2,3]]))
