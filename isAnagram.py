fc29517b1c433cf747743428d8dec2f2731d46f9
harshilkhara <kharaharshil@gmail.com>
2022-07-12 12:32:28 -0400

    Edited isAnagram.py

--- isAnagram.py
+++ isAnagram.py
@@ -1,7 +1,5 @@
 # Approach- Categorize by Count 
-
 def isAnagram(s,t):    # TC O(N)  // SC O(N)
-
 	if len(s)!=len(t):
 		return False
 
@@ -23,7 +21,6 @@ def isAnagram(s,t):    # TC O(N)  // SC O(N)
 	return False
 
 # Approach- Categorized by sorted strings 
-
 def isAnagram1(s,t):  # TC O(nlogn)  // SC O(1)
 	if len(s)!=len(t):
 		return False
@@ -33,6 +30,18 @@ def isAnagram1(s,t):  # TC O(nlogn)  // SC O(1)
 		return True
 	return False
 
+import collections
+def isAnagram2(s,t):
+	if len(s)!=len(t): 
+		return False
+	
+	sCounter=collections.Counter(s) # The ransome note approach
+	for c in t:
+	    if sCounter[c]<=0:
+	        return False
+	    sCounter[c]-=1
+	return True
 
 print(isAnagram("anagram","nagaram"))
-print(isAnagram("car","rat"))
+print(isAnagram1("car","rat"))
+print(isAnagram2("car","acr"))
