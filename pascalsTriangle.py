05fe6b1d814369b06b492b7a616b6826888db58a
harshilkhara <kharaharshil@gmail.com>
2022-07-23 17:32:54 -0400

    Added pascalsTriangle.java & py

---
+++ pascalsTriangle.py
@@ -0,0 +1,19 @@
+def pascalsTriangle(num_rows): # TC O(n^2) // SC O(1)
+	triangle = []
+
+	for row_num in range(num_rows):
+	    # The first and last row elements are always 1.
+	    row = [None for _ in range(row_num + 1)]
+	    row[0], row[-1] = 1, 1
+
+	    # Each triangle element is equal to the sum of the elements
+	    # above-and-to-the-left and above-and-to-the-right.
+	    for j in range(1, len(row) - 1):
+	        row[j] = triangle[row_num - 1][j - 1] + triangle[row_num - 1][j]
+
+	    triangle.append(row)
+
+	return triangle
+
+print(pascalsTriangle(5))
+
