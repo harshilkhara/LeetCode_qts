6288933c7b07ef1d7169d705dc1e06f9fb3fb7cf
harshilkhara <kharaharshil@gmail.com>
2022-11-28 22:57:53 -0500

    Added addBinary.java

--- addBinary.java
+++ addBinary.java
@@ -1,4 +1,5 @@
-class Solution {
+// Approach 1- Bit by bit computation
+class Solution { // TC O(max(n,m) SC O(max(n,m)
   public String addBinary(String a, String b) {
     int n = a.length(), m = b.length();
     if (n < m) return addBinary(b, a);
@@ -19,6 +20,24 @@ class Solution {
     sb.reverse();
 
     return sb.toString();
+  }
+}
+
+// Approach 2- Bit manipulation 
+import java.math.BigInteger;
+class Solution { // TC O(m+n) SC O(max(n,m)
+  public String addBinary(String a, String b) {
+    BigInteger x = new BigInteger(a, 2);
+    BigInteger y = new BigInteger(b, 2);
+    BigInteger zero = new BigInteger("0", 2);
+    BigInteger carry, answer;
+    while (y.compareTo(zero) != 0) {
+      answer = x.xor(y);
+      carry = x.and(y).shiftLeft(1);
+      x = answer;
+      y = carry;
+    }
+    return x.toString(2);
   }
 }
 
