78656b687cd87b4efaff6af177deb984718e06d5
harshilkhara <kharaharshil@gmail.com>
2023-01-16 01:40:03 -0500

    Added insertNewInterval.java

---
+++ insertNewInterval.java
@@ -0,0 +1,140 @@
+import java.util.*;
+// Approach 1- Linear Search
+class Solution { // TC O(N) SC O(1)
+    // Returns true if the intervals a and b have a common element.
+    boolean doesIntervalsOverlap(int[] a, int[] b) {
+        return Math.min(a[1], b[1]) - Math.max(a[0], b[0]) >= 0;
+    }
+
+    // Return the interval having all the elements of intervals a and b.
+    int[] mergeIntervals(int[] a, int[] b) {
+        int[] newInterval = {Math.min(a[0], b[0]), Math.max(a[1], b[1])};
+        return newInterval;
+    }
+
+    // Insert the interval newInterval, into the list interval keeping the sorting order intact.
+    int[][] insertInterval(int[][] intervals, int[] newInterval) {
+        boolean isIntervalInserted = false;
+        List<int[]> list = new ArrayList<>(Arrays.asList(intervals));
+
+        for (int i = 0; i < intervals.length; i++) {
+            if (newInterval[0] < intervals[i][0]) {
+                // Found the position, insert the interval and break from the loop.
+                list.add(i, newInterval);
+                isIntervalInserted = true;
+                break;
+            }
+        }
+
+        // If there is no interval with a greater value of start value,
+        // then the interval must be inserted at the end of the list.
+        if (!isIntervalInserted) {
+            list.add(newInterval);
+        }
+
+        return list.toArray(new int[list.size()][2]);
+    }
+
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        // Insert the interval first before merge processing.
+        intervals = insertInterval(intervals, newInterval);
+
+        List<int[]> answer = new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {
+            int[] currInterval = {intervals[i][0], intervals[i][1]};
+            // Merge until the list gets exhausted or no overlap is found.
+            while (i < intervals.length && doesIntervalsOverlap(currInterval, intervals[i])) {
+                currInterval = mergeIntervals(currInterval, intervals[i]);
+                i++;
+            }
+            // Decrement to ensure we don't skip the interval due to outer for-loop incrementing.
+            i--;
+            answer.add(currInterval);
+        }
+
+        return answer.toArray(new int[answer.size()][2]);
+    }
+}
+
+// Approach 2- Binary Search since the input is sorted
+class Solution1{ // TC O(n) SC O(1)
+    // Returns true if the intervals a and b have a common element.
+    boolean doesIntervalsOverlap(int[] a, int[] b) {
+        return Math.min(a[1], b[1]) - Math.max(a[0], b[0]) >= 0;
+    }
+
+    // Return the interval having all the elements of intervals a and b.
+    int[] mergeIntervals(int[] a, int[] b) {
+        int[] newInterval = {Math.min(a[0], b[0]), Math.max(a[1], b[1])};
+        return newInterval;
+    }
+
+    int UpperBound(int[][] intervals, int[] newInterval) {
+        if (intervals.length == 0) {
+            // If the list is empty, just insert the newInterval at the 0th position.
+            return 0;
+        }
+
+        int start = 0, end = intervals.length - 1;
+        int ans = intervals.length;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            // If the start value is greater than the newInterval
+            // This could be the position, so store it but keep looking on the left side.
+            if (intervals[mid][0] > newInterval[0]) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                // Search on the right side of mid.
+                start = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    // Insert the interval newInterval, into the list interval keeping the sorting order intact.
+    int[][] insertInterval(int[][] intervals, int[] newInterval) {
+        List<int[]> list = new ArrayList<>(Arrays.asList(intervals));
+        int index = UpperBound(intervals, newInterval);
+
+        if (index != intervals.length) {
+            list.add(index, newInterval);
+        } else {
+            list.add(newInterval);
+        }
+
+        return list.toArray(new int[list.size()][2]);
+    }
+
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        // Insert the interval first before merge processing.
+        intervals = insertInterval(intervals, newInterval);
+
+        List<int[]> answer = new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {
+            int[] currInterval = {intervals[i][0], intervals[i][1]};
+            // Merge until the list gets exhausted or no overlap is found.
+            while (i < intervals.length && doesIntervalsOverlap(currInterval, intervals[i])) {
+                currInterval = mergeIntervals(currInterval, intervals[i]);
+                i++;
+            }
+            // Decrement to ensure we don't skip the interval due to outer for-loop incrementing.
+            i--;
+            answer.add(currInterval);
+        }
+
+        return answer.toArray(new int[answer.size()][2]);
+    }
+}
+
+class insertNewInterval{
+    public static void main(String[] args) {
+        int[][] intervals={{1,2},{3,5},{6,7},{8,10},{12,16}};
+        int[] newInterval={4,8};
+        Solution s= new Solution();
+        System.out.println(Arrays.deepToString(s.insert(intervals, newInterval)));
+    }
+    
+}
