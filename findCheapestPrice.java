c706fdbce3f0e00d221b970b631ff6463686f65f
harshilkhara <kharaharshil@gmail.com>
2023-01-27 01:15:15 -0500

    Added findCheapestPrice.java

---
+++ findCheapestPrice.java
@@ -0,0 +1,116 @@
+import java.util.*;
+// Approach 1- BFS 
+class Solution { // TC O(n+e.k) SC O(n+e.k)
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        Map<Integer, List<int[]>> adj = new HashMap<>();
+        for (int[] i : flights)
+            adj.computeIfAbsent(i[0], value -> new ArrayList<>()).add(new int[] { i[1], i[2] });
+
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] { src, 0 });
+        int stops = 0;
+
+        while (stops <= k && !q.isEmpty()) {
+            int sz = q.size();
+            // Iterate on current level.
+            while (sz-- > 0) {
+                int[] temp = q.poll();
+                int node = temp[0];
+                int distance = temp[1];
+
+                if (!adj.containsKey(node))
+                    continue;
+                // Loop over neighbors of popped node.
+                for (int[] e : adj.get(node)) {
+                    int neighbour = e[0];
+                    int price = e[1];
+                    if (price + distance >= dist[neighbour])
+                        continue;
+                    dist[neighbour] = price + distance;
+                    q.offer(new int[] { neighbour, dist[neighbour] });
+                }
+            }
+            stops++;
+        }
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+    }
+}
+
+// Approach 2- Bellman Ford
+class Solution1{ // TC O((n+e)).k) SC O(n)
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        // Distance from source to all other nodes.
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        // Run only K+1 times since we want shortest distance in K hops
+        for (int i = 0; i <= k; i++) {
+            // Create a copy of dist vector.
+            int[] temp = Arrays.copyOf(dist, n);
+            for (int[] flight : flights) {
+                if (dist[flight[0]] != Integer.MAX_VALUE) {
+                    temp[flight[1]] = Math.min(temp[flight[1]], dist[flight[0]] + flight[2]);
+                }
+            }
+            // Copy the temp vector into dist.
+            dist = temp;
+        }
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+    }
+}
+
+// Approach 3- Dijkstra
+class Solution2{ // TC O(n + e.k.(log e.k))  SC O(n+e.k)
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        Map<Integer, List<int[]>> adj = new HashMap<>();
+        for (int[] i : flights)
+            adj.computeIfAbsent(i[0], value -> new ArrayList<>()).add(new int[] { i[1], i[2] });
+
+        int[] stops = new int[n];
+        Arrays.fill(stops, Integer.MAX_VALUE);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        // {dist_from_src_node, node, number_of_stops_from_src_node}
+        pq.offer(new int[] { 0, src, 0 });
+
+        while (!pq.isEmpty()) {
+            int[] temp = pq.poll();
+            int dist = temp[0];
+            int node = temp[1];
+            int steps = temp[2];
+            // We have already encountered a path with a lower cost and fewer stops,
+            // or the number of stops exceeds the limit.
+            if (steps > stops[node] || steps > k + 1)
+                continue;
+            stops[node] = steps;
+            if (node == dst)
+                return dist;
+            if (!adj.containsKey(node))
+                continue;
+            for (int[] a : adj.get(node)) {
+                pq.offer(new int[] { dist + a[1], a[0], steps + 1 });
+            }
+        }
+        return -1;
+    }
+}
+
+
+class findCheapestPrice{
+    public static void main(String[] args) {
+        int n= 4;
+        int[][] flights={{0,1,100},{0,1,100},{2,0,100},{1,3,600},{2,3,200}};
+        int src=0;
+        int dst=3;
+        int k=1; 
+        Solution s= new Solution();
+        System.out.println(s.findCheapestPrice(n,flights,src,dst,k));
+        Solution1 s1= new Solution1();
+        System.out.println(s1.findCheapestPrice(n,flights,src,dst,k));
+        Solution2 s2= new Solution2();
+        System.out.println(s2.findCheapestPrice(n,flights,src,dst,k));
+    }
+}
