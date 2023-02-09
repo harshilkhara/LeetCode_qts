d999b77309d500c06fdc94a338bcd26c29be2b18
harshilkhara <kharaharshil@gmail.com>
2023-02-08 21:38:17 -0500

    Added distinctNames.py

---
+++ distinctNames.py
@@ -0,0 +1,22 @@
+# Approach 1- Group words by their initial
+def distinctNames(ideas): # TC O(n*m) // SC O(n*m)
+    # Group idea by their initials.
+    initial_groups = [set() for _ in range(26)]
+    for idea in ideas:
+        initial_groups[ord(idea[0]) - ord('a')].add(idea[1:])
+
+    answer = 0
+    # Calculate number of valid names from every pair of groups.
+    for i in range(25):
+        for j in range(i + 1, 26):
+            # Get the number of mutual suffixes.
+            num_of_mutual = len(initial_groups[i] & initial_groups[j]) 
+            
+            # Valid names are only from distinct suffixes in both groups.
+            # Since we can swap a with b and swap b with a to 
+            # create two valid names, multiple answer by 2.
+            answer += 2 * (len(initial_groups[i]) - num_of_mutual) * (len(initial_groups[j]) - num_of_mutual)
+            
+    return answer
+
+print(distinctNames(["coffee","donuts","time","toffee"]))
