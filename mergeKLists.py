a5884c310f1fd29805ab2276e4b3909f3d83f584
harshilkhara <kharaharshil@gmail.com>
2023-03-12 19:48:59 -0400

    Edited mergeKLists.py

--- mergeKLists.py
+++ mergeKLists.py
@@ -19,6 +19,33 @@ class Solution:
             node=node.next 
         node.next=None
         return head.next
+
+# Approach 2- Merge with divide and conquer
+    def mergeKLists(self, lists): # TC O(n log k) // SC O(1)
+        amount = len(lists)
+        interval = 1
+        while interval < amount:
+            for i in range(0, amount - interval, interval * 2):
+                lists[i] = self.merge2Lists(lists[i], lists[i + interval])
+            interval *= 2
+
+        return lists[0] if amount > 0 else None
+
+    def merge2Lists(self, l1, l2):
+        head = point = ListNode(0)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                point.next = l1
+                l1 = l1.next
+            else:
+                point.next = l2
+                l2 = l1
+                l1 = point.next.next
+            point = point.next
+
+        point.next=l1 if l1 else l2
+
+        return head.next
             
 list1=ListNode(1)
 list1=ListNode(4)
