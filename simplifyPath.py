026b287455a429f564d8429242f547d163e14fdd
harshilkhara <kharaharshil@gmail.com>
2023-04-12 15:24:30 -0400

    Added simplifyPath.java & .py

---
+++ simplifyPath.py
@@ -0,0 +1,30 @@
+class Solution:
+# Approach 1- Using stacks
+    def simplifyPath(self, path): # TC O(n) // SC O(n)
+
+        # Initialize a stack
+        stack = []
+
+        # Split the input string on "/" as the delimiter
+        # and process each portion one by one
+        for portion in path.split("/"):
+
+            # If the current component is a "..", then
+            # we pop an entry from the stack if it's non-empty
+            if portion == "..":
+                if stack:
+                    stack.pop()
+            elif portion == "." or not portion:
+                # A no-op for a "." or an empty string
+                continue
+            else:
+                # Finally, a legitimate directory name, so we add it
+                # to our stack
+                stack.append(portion)
+
+        # Stich together all the directory names together
+        final_str = "/" + "/".join(stack)
+        return final_str
+
+s=Solution()
+print(s.simplifyPath("/home/"))
