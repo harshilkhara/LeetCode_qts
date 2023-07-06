46a8522711fd8e99b1f1b8bcb61da6a2c97e17ee
harshilkhara <kharaharshil@gmail.com>
2023-07-05 21:43:01 -0400

    Added buddyStrings.java &.py

---
+++ buddyStrings.py
@@ -0,0 +1,40 @@
+class Solution:
+# Approach 1- Check for the mismatch
+    def buddyStrings(self, s, goal): # TC O(n) // SC O(1)
+        if len(s) != len(goal):
+            return False
+
+        if s == goal:
+            # If we have 2 same characters in string 's',
+            # we can swap them and still the strings will remain equal.
+            frequency = [0] * 26
+            for ch in s:
+                frequency[ord(ch) - ord('a')] += 1
+                if frequency[ord(ch) - ord('a')] == 2:
+                    return True
+            # Otherwise, if we swap any two characters, it will make the strings unequal.
+            return False
+
+        firstIndex = -1
+        secondIndex = -1
+
+        for i in range(len(s)):
+            if s[i] != goal[i]:
+                if firstIndex == -1:
+                    firstIndex = i
+                elif secondIndex == -1:
+                    secondIndex = i
+                else:
+                    # We have at least 3 indices with different characters,
+                    # thus, we can never make the strings equal with only one swap.
+                    return False
+
+        if secondIndex == -1:
+            # We can't swap if the character at only one index is different.
+            return False
+
+        # All characters of both strings are the same except two indices.
+        return s[firstIndex] == goal[secondIndex] and s[secondIndex] == goal[firstIndex]
+
+s=Solution()
+print(s.buddyStrings("ab","ba"))
