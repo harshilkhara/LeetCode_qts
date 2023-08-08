4ce017a1c12f5cf4c5b7ad662ab49e90a02a4eb2
harshilkhara <kharaharshil@gmail.com>
2023-08-08 18:05:45 -0400

    Edited wordBreak.java

--- wordBreak.java
+++ wordBreak.java
@@ -28,21 +28,64 @@ class Solution1 { // TC O(n^3) SC O(n)
     }
 }
 
-// Approach 3- DP 
-class Solution2{ // TC O(n^3) SC O(n)
+// Approach 3- Top down DP 
+class Solution2{ // TC O(n.m.k) SC O(n)
+    private String s;
+    private List<String> wordDict;
+    private int[] memo;
+    
+    private boolean dp(int i) {
+        if (i < 0) return true;
+        
+        if (memo[i] != -1) {
+            return memo[i] == 1;
+        }
+        
+        for (String word: wordDict) {
+            // Handle out of bounds case
+            if (i - word.length() + 1 < 0) {
+                continue;
+            }
+            
+            if (s.substring(i - word.length() + 1, i + 1).equals(word) && dp(i - word.length())) {
+                memo[i] = 1;
+                return true;
+            }
+        }
+        
+        memo[i] = 0;
+        return false;
+    }
+    
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> wordDictSet = new HashSet<>(wordDict);
-        boolean[] dp = new boolean[s.length() + 1];
-        dp[0] = true;
-        for (int i = 1; i <= s.length(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
-                    dp[i] = true;
-                    break;
+        this.s = s;
+        this.wordDict = wordDict;
+        this.memo = new int[s.length()];
+        Arrays.fill(this.memo, -1);
+        return dp(s.length() - 1);
+    }
+}
+
+// Approach 4- Bottom down DP
+class Solution3{ // TC O(m.n.k) SC O(n)
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            for (String word: wordDict) {
+                // Handle out of bounds case
+                if (i < word.length() - 1) {
+                    continue;
+                }
+                
+                if (i == word.length() - 1 || dp[i - word.length()]) {
+                    if (s.substring(i - word.length() + 1, i + 1).equals(word)) {
+                        dp[i] = true;   
+                        break;
+                    }
                 }
             }
         }
-        return dp[s.length()];
+        return dp[s.length() - 1];
     }
 }
 
